284cb5fd3b411f82a94dc1f47370aba36e8e55a1
Anuj-rana-9034 <anujrajput9034@gmail.com>
2024-04-25 11:28:47 +0530

    title change

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = () => {
         </div>
         <div>
           <h1 className="sm:text-[45px]  text-[30px] font-extrabold text-center sm:mt-[140px] mt-[70px]">The biggest Indian hits. The best Indian stories. All streaming here.</h1>
-          <p className="sm:text-[25px] text-[18px] sm:mt-6 sm:my-0 my-5 text-center ">Watch anywhere. Cancel anytime.</p>
+          <p className="sm:text-[25px] text-[18px] sm:mt-6 sm:my-0 my-5 text-center ">Abc</p>
           <p className="sm:text-[20px] text-[18px] sm:mt-6 sm:px-0 px-7  text-center ">Ready to watch? Enter your email to create or restart your membership.</p>
         </div>
         <div>
